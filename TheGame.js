94e7330f0ad7d9e9a66f5726ed087347ed0919c0
amaury.bariety@epitech.eu <amaury.bariety@epitech.eu>
2023-10-08 19:17:32 +0200

    Feat: Exercice 4 : add most rich dude

--- TheGame.js
+++ TheGame.js
@@ -4,19 +4,20 @@
 // On Linux FEDORA 35
 // Have a nice day
 
+// Data list
 const data = [
-    {id: 0,     name: 'Sergio',     age: 18,    dept: [2]},
-    {id: 1,     name: 'Maxime',     age: 52,    dept: [4,8]},
-    {id: 2,     name: 'Aurelien',   age: 16,    dept: [1,11]},
-    {id: 3,     name: 'Anthony',    age: 19,    dept: [9,5,6,2,1]},
-    {id: 4,     name: 'Emma',       age: 87,    dept: [10,6,9,8]},
-    {id: 5,     name: 'Amanda',     age: 56,    dept: []},
-    {id: 6,     name: 'Julien',     age: 11,    dept: [3]},
-    {id: 7,     name: 'David',      age: 23,    dept: [5]},
-    {id: 8,     name: 'Florian',    age: 38,    dept: [7,10]},
-    {id: 9,     name: 'Nathan',     age: 56,    dept: [4,6]},
-    {id: 10,    name: 'Thibaut',    age: 29,    dept: []},
-    {id: 11,    name: 'Hugo',       age: 18,    dept: [2,6]}
+    { id: 0, name: 'Sergio', age: 18, dept: [2] },
+    { id: 1, name: 'Maxime', age: 52, dept: [4, 8] },
+    { id: 2, name: 'Aurelien', age: 16, dept: [1, 11] },
+    { id: 3, name: 'Anthony', age: 19, dept: [9, 5, 6, 2, 1] },
+    { id: 4, name: 'Emma', age: 87, dept: [10, 6, 9, 8] },
+    { id: 5, name: 'Amanda', age: 56, dept: [] },
+    { id: 6, name: 'Julien', age: 11, dept: [3] },
+    { id: 7, name: 'David', age: 23, dept: [5] },
+    { id: 8, name: 'Florian', age: 38, dept: [7, 10] },
+    { id: 9, name: 'Nathan', age: 56, dept: [4, 6] },
+    { id: 10, name: 'Thibaut', age: 29, dept: [] },
+    { id: 11, name: 'Hugo', age: 18, dept: [2, 6] }
 ];
 
 const main = () => {
@@ -30,6 +31,7 @@ const main = () => {
     let isSortedByName = false;
     let isSortedByAge = 0;
 
+    // Link to HTML
     const populateNameList = (element, persons) => {
         element.innerHTML = "";
         persons.forEach(person => {
@@ -41,32 +43,36 @@ const main = () => {
 
     // Find person with most debts
     const findPersonWithMostDebts = () => {
-        const personWithMostDebts = originalData.reduce((maxDebtsPerson, currentPerson) => {
-            if (currentPerson.dept.length > maxDebtsPerson.dept.length) {
-                return currentPerson;
-            } else {
-                return maxDebtsPerson;
-            }
-        }, originalData[0]);
+        let maxDebtPerson = null;
+        let maxDebtCount = 0;
 
-        return personWithMostDebts;
+        data.forEach(person => {
+            if (person.dept.length > maxDebtCount) {
+                maxDebtCount = person.dept.length;
+                maxDebtPerson = person;
+            }
+        });
+
+        return maxDebtPerson;
     };
 
     // Find person owed most meals
     const findPersonOwedMostMeals = () => {
-        const personOwedMostMeals = originalData.reduce((maxOwedMealsPerson, currentPerson) => {
-            const currentOwedMeals = currentPerson.dept.length;
-            if (currentOwedMeals > maxOwedMealsPerson.dept.length) {
-                return currentPerson;
-            } else {
-                return maxOwedMealsPerson;
-            }
-        }, originalData[0]);
+        let maxMealsPerson = null;
+        let maxMealsCount = 0;
 
-        return personOwedMostMeals;
+        data.forEach(person => {
+            const count = data.reduce((total, p) => total + (p.dept.includes(person.id) ? 1 : 0), 0);
+            if (count > maxMealsCount) {
+                maxMealsCount = count;
+                maxMealsPerson = person;
+            }
+        });
+
+        return maxMealsPerson;
     };
 
-    // Update functions
+    // Update functions (on sort to not make them disapeer)
     const updateMostDebtsPerson = () => {
         const personWithMostDebts = findPersonWithMostDebts();
         mostDebtsPersonElement.textContent = `${personWithMostDebts.name} est la personne qui doit le plus de repas avec ${personWithMostDebts.dept.length} repas !`;
@@ -74,7 +80,7 @@ const main = () => {
 
     const updatePersonOwedMostMeals = () => {
         const personOwedMostMeals = findPersonOwedMostMeals();
-        personOwedMostMealsElement.textContent = `${personOwedMostMeals.name} est la personne à qui l'on doit le plus de repas : ${personOwedMostMeals.dept.length} repas.`;
+        personOwedMostMealsElement.textContent = `${personOwedMostMeals.name} est la personne à qui l'on doit le plus de repas : ${data.reduce((total, p) => total + (p.dept.includes(personOwedMostMeals.id) ? 1 : 0), 0)} repas.`;
     };
 
     populateNameList(nameListElement, originalData);
